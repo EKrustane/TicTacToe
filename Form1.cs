79cca2bb4696b223ad1752710446f6818eaec5bf
Ermine <ermux12@gmail.com>
2020-05-20 17:45:52 +0300

    version 17:45

--- Form1.cs
+++ Form1.cs
@@ -25,7 +25,8 @@ namespace TicTacToe
 
         private void Player_Click(object sender, EventArgs e)
         {
-
+            Label label = (Label)sender;
+            label.Text = "X";
         }
     }
 }
